bd9c22d58692e99c21cee7aee56048d8a0f09789
Adriano Rosa <info@adrianorosa.com>
2015-05-11 12:46:09 -0300

    check for changes before build site

--- build.sh
+++ build.sh
@@ -6,14 +6,27 @@
 # @author: Adriano Rosa <http://adrianorosa.com>
 # @usage: ./build.sh
 
+STATUS=`git status --porcelain`
 WEBROOT=`pwd`
 
-jekyll build
-git checkout gh-pages
-cp -r _site/ $WEBROOT
-open /Applications/GitHub.app
-# git add .
-# git commit -m "site build"
-# git push origin gh-pages
-# git checkout master
-# git push origin master
+# check for index changes before
+# checking out to the gh-pages
+if [[ ! $STATUS ]]; then
+
+  jekyll build
+  git checkout gh-pages
+  cp -r _site/ $WEBROOT
+  rm -r _site
+  # open /Applications/GitHub.app
+  git diff
+  git add .
+  git commit -m "site build"
+  # git push origin gh-pages
+  # git checkout master
+  # git push origin master
+  exit;
+
+else
+  echo "has to commit changes before site build "
+  exit;
+fi
